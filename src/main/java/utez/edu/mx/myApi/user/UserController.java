328a8f7e01ecbeeff35a5157ff373d3c266daef2
CokerAlcocer <angelalcocer@utez.edu.mx>
2024-09-23 13:45:50 -0600

    [UPDATE] Clase 23/09/24

--- src/main/java/utez/edu/mx/myApi/user/UserController.java
+++ src/main/java/utez/edu/mx/myApi/user/UserController.java
@@ -1,10 +1,23 @@
 package utez.edu.mx.myApi.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
 
+    // http://ip:port/api/user - GET
+    @GetMapping("")
+    public ResponseEntity<?> findAll() {
+        return userService.findAll();
+    }
+    // http://ip:port/api/user/{id} - GET
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable long id) {
+        return userService.findById(id);
+    }
 }
--- src/main/java/utez/edu/mx/myApi/user/UserService.java
+++ src/main/java/utez/edu/mx/myApi/user/UserService.java
@@ -2,6 +2,7 @@ package utez.edu.mx.myApi.user;
 
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -16,11 +17,24 @@ public class UserService {
     private UserRepository userRepository;
 
     public ResponseEntity<?> findAll() {
-        Map<String, Object> map = new HashMap<>();
+        Map<String, Object> body = new HashMap<>();
         List<User> list = userRepository.findAll();
 
-        if(list.size() != 0) {
-            map.put("message", "Recursos obtenidos de manera exitosa");
-        }
+        body.put("message", list.isEmpty() ? "Aún no hay registros" : "Operación exitosa");
+        body.put("status", 200);
+        body.put("data", list);
+
+        return new ResponseEntity<>(body, HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> findById(long id) {
+        Map<String, Object> body = new HashMap<>();
+        User found = userRepository.findById(id);
+
+        body.put("message", found == null ? "Registro no encontrado" : "Operación exitosa");
+        body.put("status", found == null ? 404 : 200);
+        body.put("data", found);
+
+        return new ResponseEntity<>(body, found == null ? HttpStatus.BAD_REQUEST : HttpStatus.OK);
     }
 }
